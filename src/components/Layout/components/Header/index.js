cc0d4fee3df30670d824e2f5fba1e2485d211313
= <=>
2022-07-15 17:49:06 +0700

    style for header search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,33 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
+import images from '@/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('content')}></div>
+      <div className={cx('content')}>
+        <img src={images.logo} alt="logo"></img>
+        <div className={cx('search')}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx('search__clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('search__loading')} icon={faSpinner} />
+          <button className={cx('search__button')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx('action')}></div>
+      </div>
     </header>
   );
 }
